38eca87716f72e1af38ce10781a95d6e178305ea
Mut Tulow <mtulow@icloud.com>
2022-03-14 20:22:08 -0600

    Auto Updated: Mon     22:20 Mar 14 2022

--- main.py
+++ main.py
@@ -1,27 +1,29 @@
 #!/usr/bin/env python3
 import wx
 
+def button_example():
+    class MyFrame(wx.Frame):    
+        def __init__(self):
+            super().__init__(parent=None, title='Fluid Program')
+            panel = wx.Panel(self)
 
-class MyFrame(wx.Frame):    
-    def __init__(self):
-        super().__init__(parent=None, title='Hello World')
-        panel = wx.Panel(self)
+            self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
+            my_btn = wx.Button(panel, label='Press Me', pos=(5, 55))
 
-        self.text_ctrl = wx.TextCtrl(panel, pos=(5, 5))
-        my_btn = wx.Button(panel, label='Press Me', pos=(5, 55))
+            self.Show()
+    
 
-        self.Show()
-
-
-
-
-
+    def example1():
+        app = wx.App()
+        frame = MyFrame()
+        app.MainLoop()
+    
+    example1()
 
 
 def main():
-    app = wx.App()
-    frame = MyFrame()
-    app.MainLoop()
+    button_example()
+    
 
 
 if __name__ == '__main__':
