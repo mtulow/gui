1018faa67ab3cca06fdb9f65c0c64d0b4e56cf12
Mut Tulow <mtulow@icloud.com>
2022-03-14 20:45:53 -0600

    Auto Updated: Mon     45:20 Mar 14 2022

--- main.py
+++ main.py
@@ -12,17 +12,63 @@ def button_example():
 
             self.Show()
     
-
-    def example1():
-        app = wx.App()
-        frame = MyFrame()
-        app.MainLoop()
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
     
-    example1()
+
+def sizers_example():
+
+    class MyFrame(wx.Frame):    
+        def __init__(self):
+            super().__init__(parent=None, title='Fluid Program')
+            panel = wx.Panel(self)        
+            my_sizer = wx.BoxSizer(wx.VERTICAL)        
+            self.text_ctrl = wx.TextCtrl(panel)
+            my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
+            my_btn = wx.Button(panel, label='Press Me')
+            my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
+            panel.SetSizer(my_sizer)        
+            self.Show()
+
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
+
+def event_example():
+
+    class MyFrame(wx.Frame):
+        def __init__(self):
+            super().__init__(parent=None, title='Hello World')
+            panel = wx.Panel(self)        
+            my_sizer = wx.BoxSizer(wx.VERTICAL)        
+            self.text_ctrl = wx.TextCtrl(panel)
+            my_sizer.Add(self.text_ctrl, 0, wx.ALL | wx.EXPAND, 5)        
+            my_btn = wx.Button(panel, label='Press Me')
+            my_btn.Bind(wx.EVT_BUTTON, self.on_press)
+            my_sizer.Add(my_btn, 0, wx.ALL | wx.CENTER, 5)        
+            panel.SetSizer(my_sizer)        
+            self.Show()
+
+        def on_press(self, event):
+            value = self.text_ctrl.GetValue()
+            if not value:
+                print("You didn't enter anything!")
+            else:
+                print(f'You typed: "{value}"')
+
+    
+    app = wx.App()
+    frame = MyFrame()
+    app.MainLoop()
 
 
 def main():
-    button_example()
+    # button_example()
+    # sizers_example()
+    event_example()
+
+
     
 
 
